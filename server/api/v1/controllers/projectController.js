8d759c62f6fce87774a4f124b59f3d370f101fa3
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-06-06 17:25:17 +0200

    project actions added

--- server/api/v1/controllers/projectController.js
+++ server/api/v1/controllers/projectController.js
@@ -5,6 +5,11 @@ const Project = require('../models/').projects;
 const User = require('../models').users;
 
 /**
+ * Validation
+ */
+const validateProjectInput = require('../validation/project');
+
+/**
  * Utilities
  */
 const moment = require('moment');
@@ -63,6 +68,108 @@ exports.get_projects_by_user = (req, res) => {
     .catch(err => res.json(err));
 }
 
+exports.create_and_publish_project = (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateProjectInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const projectData = {
+    user_id: req.user.id,
+    category_id: req.body.category_id,
+    project_start: req.body.project_start,
+    project_end: req.body.project_end,
+    budget: req.body.budget,
+    title: req.body.title,
+    body: req.body.body,
+    published_at: Date.now()
+  }
+  Project.create(projectData).then((project) => {
+      res.json(project);
+    })
+    .catch(err => res.json(err));
+}
+
+exports.create_and_save_project = (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateProjectInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const projectData = {
+    user_id: req.user.id,
+    category_id: req.body.category_id,
+    project_start: req.body.project_start,
+    project_end: req.body.project_end,
+    budget: req.body.budget,
+    title: req.body.title,
+    body: req.body.body,
+    published: false
+  }
+  Project.create(projectData).then((project) => {
+      res.json(project);
+    })
+    .catch(err => res.json(err));
+}
+
+exports.update_project = (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validateProjectInput(req.body)
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  Project.findOne({
+    where: {
+      id: req.params.id
+    }
+  }).then((project) => {
+    const projectFields = {
+      user_id: req.user.id,
+      category_id: req.body.category_id,
+      project_start: req.body.project_start,
+      project_end: req.body.project_end,
+      budget: req.body.budget,
+      title: req.body.title,
+      body: req.body.body,
+    }
+    if (!project) {
+      res.status(404).json({
+        notfound: 'No project found'
+      });
+    }
+
+    if (project.user_id !== req.user.id) {
+      res.status(400).json({
+        baduser: 'This is not your project'
+      });
+    }
+
+    const today = moment().format('MM-DD-YYYY');
+
+    if (project.project_start <= today) {
+      res.status(400).json({
+        denied: 'You cannot update this project anymore'
+      });
+    } else {
+      project.update(projectFields).then((project) => {
+        res.json(project);
+      })
+    }
+  })
+}
+
 exports.delete_project = (req, res) => {
   Project.findOne({
       where: {
@@ -101,7 +208,7 @@ exports.publish_unpublish_project = (req, res) => {
     const today = moment().format('MM-DD-YYYY');
 
     if (project.published) {
-      if (project.project_start >= today) {
+      if (project.project_start <= today) {
         res.status(400).json({
           nounpublish: "This project can't be unpublished anymore"
         })
